c20fdeb91d3ce177ea7891f5dff370b20da67a29
kmfb <zhaotianxion@gmail.com>
2019-03-11 00:57:59 +0800

    first commit

---
+++ server/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '7798984'
+app.config['MYSQL_DB'] = 'blog'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+CORS(app)
+
+@app.route('/api/v1/articles')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM blog.posts')
+    rv = cur.fetchall()
+    return jsonify(rv)
+
+if __name__ == '__main__':
+    config = {
+        'host': '0.0.0.0',
+        'port': 5000,
+        'debug': True,
+    }
+    app.run(**config)
+
+
+
+
